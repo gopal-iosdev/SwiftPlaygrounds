21288eb77f7f205ad834dc25c05ca53051f906b7
gopal-iosdev <gopal.iosdev@gmail.com>
2022-10-18 22:16:15 -0400

    Added Collection Types Playground

---
+++ SwiftLanguage.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ Link: https://docs.swift.org/swift-book/LanguageGuide/CollectionTypes.html
+ 
+ 
+ 
+ */
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
