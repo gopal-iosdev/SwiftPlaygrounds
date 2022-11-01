dd01db91bebf9c168da5882fada7b198ef387151
gopal-iosdev <gopal.iosdev@gmail.com>
2022-10-31 20:44:34 -0400

    Added methods playground

---
+++ SwiftLanguage.playground/Pages/Methods.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+/*
+ Source: https://docs.swift.org/swift-book/LanguageGuide/Methods.html
+ 
+ NOTE:
+    -
+ 
+ - Methods:
+ 
+ */
+
+import Foundation
+
+
+
+//: [Next](@next)
