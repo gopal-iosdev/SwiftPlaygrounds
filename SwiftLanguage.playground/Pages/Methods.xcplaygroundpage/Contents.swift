3c6e20053d2879b60674fa396fa5707ab918822c
gopal-iosdev <gopal.iosdev@gmail.com>
2022-10-31 22:34:35 -0400

    Finished Methods

--- SwiftLanguage.playground/Pages/Methods.xcplaygroundpage/Contents.swift
+++ SwiftLanguage.playground/Pages/Methods.xcplaygroundpage/Contents.swift
@@ -4,13 +4,161 @@
  Source: https://docs.swift.org/swift-book/LanguageGuide/Methods.html
  
  NOTE:
-    -
+    - Methods v/s Functions:
+        - Methods: Functions that is associated to a type.
+        - Functions: Chunks of code that do a specific task & not associated to a type.
+        - All Methods are Functions, but not vice versa.
+    - Structures and enumerations are value types. By default, the properties of a value type can’t be modified from within its instance methods.
+        - Use `mutating` before instance functions to modify the values.
+    - In Objective-C, you can define type-level methods only for Objective-C classes. In Swift, you can define type-level methods for all classes, structures, and enumerations. Each type method is explicitly scoped to the type it supports.
  
  - Methods:
+    - Methods are functions that are associated with a particular type.
+    - Classes, structures, and enumerations can all define instance methods, which encapsulate specific tasks and functionality for working with an instance of a given type.
+ 
+ - Instance Methods:
+    - Instance methods are functions that belong to instances of a particular class, structure, or enumeration.
+
+ - Type Methods:
+    - Define methods that are called on the type itself.
+    - You indicate type methods by writing the static keyword before the method’s func keyword.
+    - Classes can use the class keyword instead, to allow subclasses to override the superclass’s implementation of that method.
+    - A type method can call another type method with the other method’s name, without needing to prefix it with the type name.
  
  */
 
 import Foundation
+
+/*
+
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+    }
+    func increment(by amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+let counter = Counter()
+// the initial counter value is 0
+counter.increment()
+// the counter's value is now 1
+counter.increment(by: 5)
+// the counter's value is now 6
+counter.reset()
+
+struct Point {
+    var x = 0.0, y = 0.0
+    func isToTheRightOf(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+let somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOf(x: 1.0) {
+    print("This point is to the right of the line where x == 1.0")
+}
+
+// Mutating functions
+
+struct Point {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+var somePoint = Point(x: 1.0, y: 1.0)
+somePoint.moveBy(x: 2.0, y: 3.0)
+print("The point is now at (\(somePoint.x), \(somePoint.y))")
+
+struct Point2 {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = Point2(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+enum TriStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+// ovenLight is now equal to .high
+ovenLight.next()
+ 
+ */
+
+
+// Type Methods
+
+class SomeClass {
+    class func someTypeMethod() {
+        // type method implementation goes here
+        print("Hello Class Method")
+    }
+}
+SomeClass.someTypeMethod()
+
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel { highestUnlockedLevel = level }
+    }
+
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+
+player = Player(name: "Beto")
+if player.tracker.advance(to: 6) {
+    print("player is now on level 6")
+} else {
+    print("level 6 hasn't yet been unlocked")
+}
 
 
 
