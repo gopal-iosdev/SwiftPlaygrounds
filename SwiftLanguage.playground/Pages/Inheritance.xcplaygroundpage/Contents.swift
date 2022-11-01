8ffdfb51d405cfb732a891a013544385e91b9c51
gopal-iosdev <gopal.iosdev@gmail.com>
2022-11-01 00:20:22 -0400

    Added Inheritance

---
+++ SwiftLanguage.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+
+/*
+ Source: https://docs.swift.org/swift-book/LanguageGuide/Inheritance.html
+ 
+ NOTE:
+    -
+ 
+ - Inheritance:
+    -
+ 
+ */
+
+import Foundation
+
+// MARK: Inheritance
+
+
+
+//: [Next](@next)
