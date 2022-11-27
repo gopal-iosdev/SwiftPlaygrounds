47eb0dd2d37394e3450e590116eae34deca013e4
gopal-iosdev <gopal.iosdev@gmail.com>
2022-11-27 14:31:16 -0500

    Created Generics playground

---
+++ SwiftLanguage.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+/*
+Source: https://docs.swift.org/swift-book/LanguageGuide/Generics.html
+
+ NOTE:
+ -
+ 
+ - Generics:
+    - 
+ 
+ 
+ */
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
