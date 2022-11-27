cf2f1370285ad679e316f8fafc9a0325ac738a38
gopal-iosdev <gopal.iosdev@gmail.com>
2022-11-26 21:01:13 -0500

    Created Nested Types playground

---
+++ SwiftLanguage.playground/Pages/Nested Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+/*
+ Source: https://docs.swift.org/swift-book/LanguageGuide/TypeCasting.html
+ 
+ NOTE:
+ -
+ 
+ - Nested Types:
+    -
+ 
+ */
+
+import Foundation
+
+
+
+//: [Next](@next)
