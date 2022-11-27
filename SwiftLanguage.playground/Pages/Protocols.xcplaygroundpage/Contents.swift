72902613953e5a105fbaf3decd986892f5dce848
gopal-iosdev <gopal.iosdev@gmail.com>
2022-11-26 23:09:22 -0500

    Created Protocols playground

---
+++ SwiftLanguage.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+/*
+Source: https://docs.swift.org/swift-book/LanguageGuide/Protocols.html
+
+ NOTE:
+ -
+ 
+ - Protocols:
+    -
+
+ */
+
+import Foundation
+
+
+
+//: [Next](@next)
