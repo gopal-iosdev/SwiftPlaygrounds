d94ad1a57c2667533b4bb629b4080d86193f140c
gopal-iosdev <gopal.iosdev@gmail.com>
2022-11-26 21:57:08 -0500

    Finished Nested Types

--- SwiftLanguage.playground/Pages/Nested Types.xcplaygroundpage/Contents.swift
+++ SwiftLanguage.playground/Pages/Nested Types.xcplaygroundpage/Contents.swift
@@ -3,16 +3,62 @@
 /*
  Source: https://docs.swift.org/swift-book/LanguageGuide/TypeCasting.html
  
- NOTE:
- -
- 
  - Nested Types:
-    -
+    - Type with in a type.
+    - Enumerations are often created to support a specific class or structure’s functionality.
+    - To use a nested type outside of its definition context, prefix its name with the name of the type it’s nested within
  
  */
 
 import Foundation
 
+// MARK: Nested Types
 
+/*
+
+struct BlackjackCard {
+
+    // nested Suit enumeration
+    enum Suit: Character {
+        case spades = "♠", hearts = "♡", diamonds = "♢", clubs = "♣"
+    }
+
+    // nested Rank enumeration
+    enum Rank: Int {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+
+    // BlackjackCard properties and methods
+    let rank: Rank, suit: Suit
+    var description: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+    }
+}
+
+let theAceOfSpades = BlackjackCard(rank: .ace, suit: .spades)
+print("theAceOfSpades: \(theAceOfSpades.description)")
+
+let heartsSymbol = BlackjackCard.Suit.hearts.rawValue
+
+ */
 
 //: [Next](@next)
