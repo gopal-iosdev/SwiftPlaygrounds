3972691a8d0c1022e0f031220c304d7eff91a97e
gopal-iosdev <gopal.iosdev@gmail.com>
2022-12-24 15:21:58 -0500

    Added Unsafe Swift PLayground

---
+++ SwiftLanguage.playground/Pages/Unsafe Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+//: [Previous](@previous)
+
+/*
+Source:
+ - https://developer.apple.com/videos/play/wwdc2020/10648
+
+ NOTE:
+ -
+ 
+ 
+ - Unsafe Swift:
+    -
+ 
+ */
+
+import Foundation
+
+let value: String? = "Hello"
+
+print(value.unsafelyUnwrapped)
+
+
+
+
+//: [Next](@next)
